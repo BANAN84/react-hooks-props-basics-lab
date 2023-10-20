6f59b251424ed8ad18a1550ba34b2d4580bb43f6
banan <banan.faouri2001@gmail.com>
2023-10-21 01:46:43 +0300

    done

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,19 @@
 import React from "react";
 
-function About() {
+function About(props) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {props.bio ? ( // Use an if statement to check if 'bio' prop is present
+      <p>{props.bio}</p>
+      ) : null}
+      
+      
+      
+       
+     
+
+      
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       {/* add your <Links /> component here */}
     </div>
